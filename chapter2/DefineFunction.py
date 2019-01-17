09147471a04ccc7e67f6ca0f4aeca6ddefe26bd5
zjl <zjl@zjldeMacBook-Air.local>
2019-01-17 18:44:11 +0800

    学习函数定义

---
+++ chapter2/DefineFunction.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+def my_abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+
+print(my_abs(-3))
+
+
+# 空函数
+def nop():
+    pass  # pass 语句什么都不做，可以用来做站位符，例如没有写好如何实现，可以先放一个 pass
+
+
+nop()
